f35961a39b81500ac1969310e6c3c1c1775ac1a9
Doğukan <mail@dogukanalyanak.com>
2024-01-25 15:03:16 +0300

    v0.1.3

---
+++ Softeast.Lesson2.WebApp/Controllers/ClubController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Softeast.Lesson2.WebApp.Data;
+using Softeast.Lesson2.WebApp.Models;
+
+namespace Softeast.Lesson2.WebApp.Controllers
+{
+    public class ClubController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ClubController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            List<Club> clubs = _context.Clubs.ToList();
+            return View(clubs);
+        }
+
+        public IActionResult Detail(int id)
+        {
+         Club club = _context.Clubs.Include(async => async.Address).FirstOrDefault(c => c.Id == id);
+            return View(club); 
+        }
+    }
+}
---
+++ Softeast.Lesson2.WebApp/Controllers/RaceController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Softeast.Lesson2.WebApp.Data;
+using Softeast.Lesson2.WebApp.Models;
+
+namespace Softeast.Lesson2.WebApp.Controllers
+{
+    public class RaceController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public RaceController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public ApplicationDbContext Context { get; }
+
+        public IActionResult Index()
+        {
+            List<Race> races = _context.Races.ToList();
+            return View(races);
+        }
+
+        public IActionResult Detail(int id)
+        {
+            Race race = _context.Races.Include(async => async.Address).FirstOrDefault(c => c.Id == id);
+            return View(race);
+        }
+    }
+}
