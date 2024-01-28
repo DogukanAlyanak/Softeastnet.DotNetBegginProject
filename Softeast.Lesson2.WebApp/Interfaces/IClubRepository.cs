fe331033dbf55e217fa9ebf0cf50522dddf205bf
DogukanAlyanak <mail@dogukanalyanak.com>
2024-01-28 21:45:10 +0300

    v0.1.4

---
+++ Softeast.Lesson2.WebApp/Interfaces/IClubRepository.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using Softeast.Lesson2.WebApp.Models;
+
+namespace Softeast.Lesson2.WebApp.Interfaces
+{
+    public interface IClubRepository
+    {
+        Task<IEnumerable<Club>> GetAll();
+        Task<Club> GetByIdAsync(int id);
+        Task<IEnumerable<Club>> GetClubByCity(string city);
+        bool Add(Club club);
+        bool Update(Club club);
+        bool Delete(Club club);
+        bool Save();
+    }
+}
